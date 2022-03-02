6c42d7ce6ef2190e56c0f28bef971c5b1e9cd6c8
moon <moonheejoo49@gmail.com>
2022-03-02 21:04:22 +0900

    add ValueError and max_split

--- RS.py
+++ RS.py
@@ -1,7 +1,6 @@
 import FinanceDataReader as fdr
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
 
 def cal_R_S(df):
@@ -12,14 +11,17 @@ def cal_R_S(df):
     return R/S
 
 
-def hurst_exponent(df):
+def hurst_exponent(df, max_split):
     size = []
     R_S = []
 
     if len(df) < 30:
         raise ValueError('A time series is too short. Length must be more than 30')
 
-    for i in range(30, len(df/2)):
+    if max_split >= len(df):
+        raise ValueError('max_split must be smaller than length of data')
+
+    for i in range(2, max_split):
         tmp_list = []
 
         for j in np.array_split(df, i):
@@ -32,24 +34,7 @@ def hurst_exponent(df):
     return results[0]
 
 
-def time_series_hurst_expo(n, df, range_):
-    '''
-
-    :param n: (int) period of time series
-    :param j: (int) time interval
-    :param df: (pd.Series) Close price
-    :param range_: R/S range
-    :return:
-    '''
-    h_df = pd.DataFrame(columns=['hurst_exp'])
-    for i in range(0, len(df)-n):
-        h_df.loc[df[i:i+n].index[-1]] = hurst_exponent(range_, df[i:i+n])
-
-    return h_df
-
-
 if __name__ == '__main__':
-    df = fdr.DataReader('US500')
-    data = df.Close
-    range_ = [2, 4, 8, 16, 32, 64]
-    h_df = time_series_hurst_expo(128, data, range_)
+    us500 = fdr.DataReader('US500', '2010-01-01')
+    splits = []
+
