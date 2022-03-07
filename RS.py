f97091a7b544b12479f4abde0ca150ee57e670e1
moon <moonheejoo49@gmail.com>
2022-03-07 21:02:22 +0900

    modified

--- RS.py
+++ RS.py
@@ -1,17 +1,22 @@
 import FinanceDataReader as fdr
-import pandas as pd
 import numpy as np
+from typing import Union
+import pandas as pd
 
 
-def cal_R_S(df):
-    z = np.cumsum(df - df.mean())
+def cal_R_S(df: np.array):
+
+    df = np.log(df)
+    z = np.cumsum(df - np.mean(df))
     R = np.max(z) - np.min(z)
-    S = df.std()
+    S = np.std(df)
 
     return R/S
 
 
-def hurst_exponent(df, max_split):
+def hurst_exponent(df: Union[np.array, pd.Series],
+                   max_split: Union[int, list],
+                   ):
     size = []
     R_S = []
 
@@ -27,14 +32,22 @@ def hurst_exponent(df, max_split):
         for j in np.array_split(df, i):
             tmp_list.append(cal_R_S(j))
         R_S.append(np.log(np.mean(tmp_list)))
-        size.append(np.log(len(j)))
+        size.append(np.log(len(j)/2))
 
     results = np.polyfit(size, R_S, 1)
 
-    return results[0]
+    if results[0] < 0 or results[0] > 1:
+        raise ResultsError
+
+    else: return results[0]
+
+class ResultsError(Exception):
+    def __init__(self):
+        super().__init__('Hurst exponent must be between 0 and 1.')
 
 
 if __name__ == '__main__':
-    us500 = fdr.DataReader('US500', '2010-01-01')
-    splits = []
-
+    us500 = fdr.DataReader('US500', '2010-01-01', '2012-12-31').Close
+    splits = [32]
+    for s in splits:
+        print('Hurst Exponent with %d splits:' %s, hurst_exponent(us500, max_split=s))
---
+++ labeling.py
@@ -0,0 +1,7 @@
+# Trend-scanning labeling and Hurst exponent labeling
+
+import numpy as np
+import pandas as pd
+from Intro import hurst_exponent
+
+def trend_scanning():
