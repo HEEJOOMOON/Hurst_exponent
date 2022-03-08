72d013d18ee6c98fe28763efb0afbd3e709cc11b
moon <moonheejoo49@gmail.com>
2022-03-08 21:09:35 +0900

    add trend-scanning labeling

--- labeling.py
+++ labeling.py
@@ -3,7 +3,6 @@
 import numpy as np
 import pandas as pd
 from Intro import hurst_exponent
-from typing import Union
 
 
 def cal_tval(df: np.array,
@@ -13,23 +12,38 @@ def cal_tval(df: np.array,
     result = np.linalg.lstsq(x, df, rcond=None)
     beta_0, beta_1 = result[0]
     se = np.sqrt(result[1]/(df.shape[0]-2)) / np.sqrt(np.sum((x[:, 1]-np.mean(x[:, 1])**2)))
-    tval = beta_1/se
+    tVal = beta_1/se
 
-    return tval
+    return tVal
 
 
-def trend_scanning_label(df: Union[pd.Series, np.array],
+def trend_scanning_label(df: pd.Series,
                          L: list,
                          ):
     '''
 
     :param df: time sereis data
-    :param L: a set of time lag l
+    :param L: a set of time lag l (start, end, step)
     :return: trend-scanning label dataframe
     '''
 
+    out = pd.DataFrame(index=df.index, columns=['t1', 'tVal', 'bin'])
+    lags = range(*L)
+    for i in df.index:
+        tmp = pd.Series()
+        idx = df.index.get_loc(i)
+        if idx+max(lags) > df.shape[0]: continue
 
-    return None
+        for l in lags:
+            end_t = idx+l-1
+            tmp.cal_tval(df.iloc[idx:end_t])
+            tmp.loc[end_t] = cal_tval(df.iloc[idx: end_t])
+        dt = tmp.replace([np.inf, -np.inf, np.nan], 0).abs().idxmax()
+        out.loc[i, ['t1', 'tVal', 'bin']] = tmp.index[-1], tmp[dt], np.sign(tmp[dt])
+
+    out['t1'] = pd.to_datetime(out['t1'])
+    out['bin'] = pd.to_numeric(out['bin'])
+    return out.dropna(subset='bin')
 
 
 def output_label():
