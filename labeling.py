d513f25856b0e1442cee71f49f08a8f77fa40cf3
moon <moonheejoo49@gmail.com>
2022-03-08 16:36:49 +0900

    modified the standard error of coef

--- labeling.py
+++ labeling.py
@@ -12,7 +12,7 @@ def cal_tval(df: np.array,
     x[:, 1] = np.arange(df.shape[0])
     result = np.linalg.lstsq(x, df, rcond=None)
     beta_0, beta_1 = result[0]
-    se = np.sqrt(result[1]/(df.shape[0]-2)) / np.sqrt(np.sum(x[:, 1]-np.mean(x[:, 1])))
+    se = np.sqrt(result[1]/(df.shape[0]-2)) / np.sqrt(np.sum((x[:, 1]-np.mean(x[:, 1])**2)))
     tval = beta_1/se
 
     return tval
@@ -21,6 +21,12 @@ def cal_tval(df: np.array,
 def trend_scanning_label(df: Union[pd.Series, np.array],
                          L: list,
                          ):
+    '''
+
+    :param df: time sereis data
+    :param L: a set of time lag l
+    :return: trend-scanning label dataframe
+    '''
 
 
     return None
